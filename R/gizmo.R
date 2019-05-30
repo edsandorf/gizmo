76bfb9ab216375dc15ba03b268eb1db9a9e01aed
edsandorf <esandorf@gmail.com>
2019-05-30 15:32:43 +0100

    Added package level documentation

---
+++ R/gizmo.R
@@ -0,0 +1,9 @@
+#' gizmo: A supplementary package for statistical analysis and choice modeling
+#' 
+#' The gizmo package provides several useful functions for statistical analysis
+#' and choice modeling.
+#' 
+#' 
+#' @docType package
+#' @name gizmo
+NULL
