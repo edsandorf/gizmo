d2d0fe0579ce6992ab869a5686e2d25d742db71a
edsandorf <esandorf@gmail.com>
2019-05-31 09:51:44 +0100

    updated on_attach()

--- R/on_attach.R
+++ R/on_attach.R
@@ -8,5 +8,10 @@
 #' @return Nothing
 
 .onAttach <- function(libname, pkgname){
-    packageStartupMessage("You are currently using gizmo")
+    pkg_version <- utils::packageDescription("gizmo", fields = "Version")
+    
+    packageStartupMessage("Thank you for using gizmo! \n\n",
+                          "You are currently using version: ", pkg_version, "\n\n",
+                          "To access the latest version, please run",
+                          "devtools::install_github('edsandorf/gizmo') \n\n")
 }
