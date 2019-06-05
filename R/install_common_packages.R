e1df11e87629f7989c7969e4f62000f46556c8ed
edsandorf <esandorf@gmail.com>
2019-06-05 21:28:28 +0100

    Added install_common_packages()

---
+++ R/install_common_packages.R
@@ -0,0 +1,19 @@
+#' Wrapper function to install commonly used packages
+#' 
+#' The function should only be called with a fresh install of R. It will install
+#' all packages I have added to the list. The function serves no other purpose.
+#' 
+#' @export
+
+install_common_packages <- function() {
+  packages <- c("devtools", "packrat", "rsconnect", "config", "roxygen2",
+                "testthat", "tidyverse", "pryr", "feather", "shiny", "shinyjs",
+                "shinyWidgets", "aws.s3", "RMariaDB", "janitor", "data.table",
+                "Rfast", "maxLik", "trustOptim", "nloptr", "numDeriv",
+                "randtoolbox", "matrixStats", "evd", "apollo", "gmnl",
+                "carthography", "sf", "AER", "car", "psych", "MatchIt",
+                "knitr", "komadown", "xtable", "tikzDevice", "formatR",
+                "swirl", "microbenchmark", "installr", "fortune", "beepr")
+  
+  utils::install.packages(packages, repos = "https://cloud.r-project.org")
+}
