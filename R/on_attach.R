fe75d2dbd6a5b2cf0670c0a78c56b147276284be
edsandorf <esandorf@gmail.com>
2019-06-11 16:11:04 +0100

    wrapped in tryCatch()

--- R/on_attach.R
+++ R/on_attach.R
@@ -10,8 +10,19 @@
 .onAttach <- function(libname, pkgname){
     installed_version <- utils::packageDescription("gizmo", fields = "Version")
     
-    description <- readLines("https://raw.githubusercontent.com/edsandorf/gizmo/master/DESCRIPTION")
-    remote_version <- gsub("Version:\\s*", "", description[grep('Version:', description)])
+    description <- tryCatch({
+        readLines("https://raw.githubusercontent.com/edsandorf/gizmo/master/DESCRIPTION")
+    }, warning = function(w) {
+        return("NA")
+    }, error = function(e) {
+        return("NA")
+    })
+    
+    if (description == "NA") {
+        remote_version <- description
+    } else {
+        remote_version <- gsub("Version:\\s*", "", description[grep('Version:', description)])    
+    }
     
     packageStartupMessage("Thank you for using gizmo! \n\n",
                           "You are currently using version: ",
