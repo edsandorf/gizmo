fad66e1571055700ce38fef355ad1167d1c1edd8
Erlend Dancke Sandorf <edsandorf@gmail.com>
2021-06-22 09:08:56 +0200

    Added rep to gizmo. Will eventually be removed from other packages.

---
+++ tests/testthat/test-rep.R
@@ -0,0 +1,23 @@
+context("Test rep_*")
+
+x <- matrix(1:4, nrow = 2)
+
+test_that("Repeat rows correctly repeats the rows of a matrix", {
+  expect_equal(
+    rep_rows(x, 2),
+    structure(
+      c(1L, 1L, 2L, 2L, 3L, 3L, 4L, 4L),
+      .Dim = c(4L, 2L)
+    )
+  )
+})
+
+test_that("Repeat columns correctly repeats the columns of a matrix", {
+  expect_equal(
+    rep_cols(x, 2),
+    structure(
+      c(1L, 2L, 1L, 2L, 3L, 4L, 3L, 4L),
+      .Dim = c(2L, 4L)
+    )
+  )
+})
